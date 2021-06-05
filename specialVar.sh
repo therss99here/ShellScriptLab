e8271087e6db1955e10b2689f3da192362707b29
therss99here <rahulsinghsengar009@gmail.com>
2021-06-05 16:37:54 +0530

    Special Variables in Shell Script

---
+++ specialVar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NAME="Rahul S Sengar"
+
+echo $NAME
+echo "Test: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
